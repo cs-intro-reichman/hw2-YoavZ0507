22285abe7c9295056b6994f8fb68be5e51aaa988
Yoavz0507 <yoav.ziberski@gmail.com>
2024-11-19 00:07:56 +0200

    st

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,24 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		double pi= Math.PI;
+		int input= Integer.parseInt(args[0]);;
+		int i= 1;
+		double pii= 1-1/3;
+		while(i<input){
+			double div= 3+2*i;
+			 if(i%2 == 1){
+				pii=pii + 1/div;
+			}else if(i%2!=0){
+				pii=pii- 1/div;
+			}
+			i++;
+		}
+		double piii= pii*4;
+
+		System.out.println("pi according to Java:" + pi);
+		System.out.println("pi, approximated:" + piii);
+
+
 	}
 }
