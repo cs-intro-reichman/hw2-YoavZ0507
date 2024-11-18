c59fb860024b69f330dbf9fde38655695a139cc9
Yoavz0507 <yoav.ziberski@gmail.com>
2024-11-18 23:24:00 +0200

    first try 2

--- Cheers.java
+++ Cheers.java
@@ -2,5 +2,27 @@
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            String cheer= args[0];
+            int num= Integer.parseInt(args[1]);
+            int i= 0;
+            String l= "AEFHILMNORSX";
+
+            while(i<cheer.length()){
+                char c= cheer.charAt(i);
+                if(l.indexOf(c) != -1) {
+
+                System.out.println("Give me an " + cheer.charAt(i) + ": " + cheer.charAt(i) + "!");
+
+            }else {
+                System.out.println("Give me a " + cheer.charAt(i) + ": " + cheer.charAt(i) + "!"); 
+            }
+            i++;
+        }
+
+        for (int j= 0; j<num; j++){
+                System.out.println(cheer + "!!!");
+        }
+
+
         }
 }
